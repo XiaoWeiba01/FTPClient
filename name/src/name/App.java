be826e0495628a2620e1fb73f7b91f19f834115f
qinyq <qyqiong@qq.com>
2017-04-01 10:08:31 +0800

    设置了AppFrame

--- name/src/name/App.java
+++ name/src/name/App.java
@@ -3,8 +3,8 @@ package name;
 public class App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		AppFrame app=new AppFrame();
 	}
 
 }
--- name/src/name/AppFrame.java
+++ name/src/name/AppFrame.java
@@ -2,10 +2,22 @@ package name;
 
 import javax.swing.JFrame;
 
-public class AppFrame extends JFrame{
+public class AppFrame extends JFrame {
 	
-  public AppFrame() {
 	
-	  setSize(500,500);
-}
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * 构造方法
+	 */
+	public AppFrame() {
+
+		setTitle("五子棋");
+		setSize(500, 500);
+		setLocationRelativeTo(null);
+		setResizable(false);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setVisible(true);
+	}
+
 }
